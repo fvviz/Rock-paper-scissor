159035aca3eca7e270311d9badcf580584b4957a
faiz <faizrahimbusiness@gmail.com>
2020-12-17 18:19:39 +0530

    fixed bug in game

--- _utils.py
+++ _utils.py
@@ -3,6 +3,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 
 import cv2
 import numpy as np
+import time
 
 from game_utils import Gesture
 from game_utils import RockPaperScissors
@@ -68,6 +69,8 @@ class WebCam:
         computer_gesture = Gesture.generate_random()
         scores = [0, 0, 0]
         hand_in_screen = 0
+        result_ = ""
+        frames_elapsed = 0
 
         while cap.isOpened():
             ret, frame = cap.read()
@@ -77,30 +80,36 @@ class WebCam:
             frame = cv2.flip(frame, 1)
 
             if not gesture == "empty":
-                person_gesture = Gesture(gesture)
-                image = cv2.imread(computer_gestures[computer_gesture.name])
-                image = cv2.resize(image, (180, 180))
-                x_offset, y_offset = (350, 100)
-                frame[y_offset:y_offset + image.shape[0], x_offset:x_offset + image.shape[1]] = image
-                result = RockPaperScissors.get_result(person_gesture, computer_gesture)
+                frames_elapsed += 1
+                if frames_elapsed > 5:
+                    person_gesture = Gesture(gesture)
+                    image = cv2.imread(computer_gestures[computer_gesture.name])
+                    image = cv2.resize(image, (180, 180))
+                    x_offset, y_offset = (350, 100)
+                    frame[y_offset:y_offset + image.shape[0], x_offset:x_offset + image.shape[1]] = image
+                    result = RockPaperScissors.get_result(person_gesture, computer_gesture)
 
-                if hand_in_screen == 0:
-                    scores[result[0]] += 1
-                    hand_in_screen += 1
+                    if hand_in_screen == 0:
+                        scores[result[0]] += 1
+                        hand_in_screen += 1
+                        result_ = result[1]
 
-                cls.create_text(frame, f"result: {result[1]}", org=(100, 350), color=(255, 255, 255))
-
+                    cls.create_text(frame, f"result: {result_}", org=(100, 350), color=(255, 255, 255))
+                    frames_elapsed += 1
             else:
                 computer_gesture = Gesture.generate_random()
                 hand_in_screen = 0
+                frames_elapsed = 0
 
             cls.create_text(frame, f"Person: {scores[0]}", org=(100, 310), color=(255, 0, 0))
             cls.create_text(frame, f"Computer: {scores[1]}", org=(350, 310), color=(255, 0, 0))
             cls.create_text(frame, f"{gesture} {percent}%", org=(100, 100))
+            cls.create_text(frame, f"frames: {frames_elapsed}", org=(100, 390), color=(0, 0, 0), font_scale=1, thickness=2)
             frame = cv2.resize(frame, (1000, 700))
             cv2.imshow('Rock Paper Scissors!', frame)
             if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
                 break
+
 
     @classmethod
     def start(cls):
